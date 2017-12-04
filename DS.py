706f1660b98e2604b936dd4db2513e3a2379e1f7
Ali Daghighi <daghighi1376@gmail.com>
2017-12-04 17:09:14 +0330

    Data structure

---
+++ DS.py
@@ -0,0 +1,52 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+        self.prev = None
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def add(self, data):
+        node = Node(data)
+        if self.head is None:
+            self.head = node
+        else:
+            node.next = self.head
+            node.next.prev = node
+            self.head = node
+
+    def search(self, k):
+        p = self.head
+        if p is not None:
+            while p.next is not None:
+                if p.data is k:
+                    return p
+                p = p.next
+            if p.data is k:
+                return p
+        return None
+
+    def remove(self, p):
+        tmp = p.prev
+        p.prev.next = p.next
+        p.prev = tmp
+
+    def str(self):
+        s = ""
+        p = self.head
+        if p is not None:
+            while p.next is not None:
+                s += p.data
+                p = p.next
+            s += p.data
+        return s
+
+    def top(self):
+        s = ""
+        p = self.head
+        s += p.data
+        return s
+
