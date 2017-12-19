d3060ecb9454e8bb4724a59d00491d0d73bf05e8
Ali Daghighi <daghighi1376@gmail.com>
2017-12-19 04:51:06 +0330

    changing Main and DS

--- DS.py
+++ DS.py
@@ -1,4 +1,4 @@
-
+import time
 
 """
 We have Node class:
@@ -20,11 +20,29 @@ class Node:
         self.next = None
         self.prev = None
         self.child = None
+        self.list = []
         self.car_model = car_model
         self.costumer_description = costumer_description
         self.agency_description = agency_description
         self.price = price
         self.agency_name = agency_name
+
+        self.high_heap = MaxHeap()
+        self.med_heap = MaxHeap()
+        self.low_heap = MaxHeap()
+
+    def __init__(self, service_name, priority_level, customer_name, agency_name):
+        self.service = service_name
+        self.customer = customer_name
+        self.agency = agency_name
+        self.priority = priority_level
+
+        self.time = time.time() * -1
+
+    def __str__(self):
+        print('- Customer "{}" has requested service "{}" from agency "{}" with "{}" priority'.format(
+            self.customer, self.service, self.agency, self.priority)
+        )
 
 
 """
@@ -41,7 +59,7 @@ class LinkedList:
     def __init__(self):
         self.head = None
 
-    def add(self, data, child=None):
+    def add(self, data):
         node = Node(data)
         if self.head is None:
             self.head = node
@@ -49,9 +67,39 @@ class LinkedList:
             node.next = self.head
             node.next.prev = node
             self.head = node
+    
+    def add_sub_service(self, sub_service_name, service_name):
+        if self.search(sub_service_name) is not None:
+            print('Err! already added!')
+            return
 
-        if child is not None:
-            self.head.child = child
+        temp = Node(sub_service_name)
+        p = self.search(service_name)
+
+        if p is None:
+            print('Err! check your service')
+
+        if p.child is not None:
+            p = p.child
+
+            while p.next is not None:
+                p = p.next
+
+            p.next = temp
+
+        else:
+            p.child = temp
+        
+    def add_offer(self, service_name, agency_name):
+        p = self.head
+
+        while p.service_name != agency_name:
+            p = p.next
+
+        if service_name not in p.list:
+            p.list.append(service_name)
+        else:
+            print('Already added!')
 
     def search(self, k):
         p = self.head
@@ -66,10 +114,19 @@ class LinkedList:
                 return p
         return None
 
-    def remove(self, p):
-        tmp = p.prev
-        p.prev.next = p.next
-        p.prev = tmp
+    def delete(self, service_name, agency_name):
+        if self.head is None:
+            print('Err! nothing found to delete')
+            return
+        p = self.head
+        while p.service_name != agency_name:
+            p = p.next
+
+        if service_name in p.list:
+            p.list.remove(service_name)
+
+        else:
+            print('Err! there is no such service!')
 
     def show(self):
         s = ""
@@ -82,6 +139,45 @@ class LinkedList:
                 p = p.next
             s += (str(p.data))
         return s
+
+    def _order(self, service_name, agency_name, customer_name, priority_level):
+        agency = self.search(agency_name)
+        if agency is None:
+            print('Err!')
+
+        p = Node(service_name, priority_level, customer_name, agency_name)
+
+        if priority_level == 'high':
+            agency.high_heap.push(p)
+
+        elif priority_level == 'normal':
+            agency.med_heap.push(p)
+
+        elif priority_level == 'low':
+            agency.low_heap.push(p)
+
+        else:
+            print('Err!')
+            
+    def orders_list(self, agency_name):
+        agency = self.search(agency_name)
+
+        if agency is None:
+            print('Err!')
+
+        if agency.high_heap.__len__() == agency.med_heap.__len__() == agency.low_heap.__len__() == 0:
+            print('There are no orders right now!')
+
+        while agency.high_hap.__len__() != 0:
+            print(agency.high_hap.pop())
+
+        while agency.med_hap.__len__() != 0:
+            print(agency.med_hap.pop())
+
+        while agency.low_hap.__len__() != 0:
+            print(agency.low_hap.pop())
+
+
 
 """
 MaxHeap:
@@ -104,6 +200,9 @@ class MaxHeap:
         for i in items:
             self.heap.append(i)
             self.float_up(len(self.heap) - 1)
+
+    def __len__(self):
+        return len(self.heap)
 
     def push(self, data):
         self.heap.append(data)
@@ -136,7 +235,7 @@ class MaxHeap:
         parent = index // 2
         if index <= 1:
             return
-        elif self.heap[index] > self.heap[parent]:
+        elif self.heap[index].time > self.heap[parent].time:
             self.swap(index, parent)
             self.float_up(parent)
 
@@ -144,74 +243,133 @@ class MaxHeap:
         left = index * 2
         right = index * 2 + 1
         largest = index
-        if len(self.heap) > left and self.heap[largest] < self.heap[left]:
+        if len(self.heap) > left and self.heap[largest].time < self.heap[left].time:
             largest = left
-        if len(self.heap) > right and self.heap[largest] < self.heap[right]:
+        if len(self.heap) > right and self.heap[largest].time < self.heap[right].time:
             largest = right
         if largest != index:
             self.swap(index, largest)
             self.bubble_down(largest)
 
-    def get_flag(self):
-        return self.heap[len(self.heap) - 1]
-
-
-def _hash(_string):
+"""
+def _encode(_string):
     s = ''
     for i in _string:
         print(i)
         if i in '':
-            s += '00'
+            s += '00 '
         elif i in 'A':
-            s += '01'
+            s += '01 '
         elif i in 'B':
-            s += '02'
+            s += '02 '
         elif i in 'C':
-            s += '03'
+            s += '03 '
         elif i in 'D':
-            s += '04'
+            s += '04 '
         elif i in 'E':
-            s += '05'
+            s += '05 '
         elif i in 'F':
-            s += '06'
+            s += '06 '
         elif i in 'G':
-            s += '07'
+            s += '07 '
         elif i in 'H':
-            s += '08'
+            s += '08 '
         elif i in 'I':
-            s += '09'
+            s += '09 '
         elif i in 'J':
-            s += '10'
+            s += '10 '
         elif i in 'K':
-            s += '11'
+            s += '11 '
         elif i in 'L':
-            s += '12'
+            s += '12 '
         elif i in 'M':
-            s += '13'
+            s += '13 '
         elif i in 'N':
-            s += '14'
+            s += '14 '
         elif i in 'O':
-            s += '15'
+            s += '15 '
         elif i in 'P':
-            s += '16'
+            s += '16 '
         elif i in 'Q':
-            s += '17'
+            s += '17 '
         elif i in 'R':
-            s += '18'
+            s += '18 '
         elif i in 'S':
-            s += '19'
+            s += '19 '
         elif i in 'T':
-            s += '20'
+            s += '20 '
         elif i in 'U':
-            s += '21'
+            s += '21 '
         elif i in 'V':
-            s += '22'
+            s += '22 '
         elif i in 'W':
-            s += '24'
+            s += '23 '
         elif i in 'X':
-            s += '25'
+            s += '24 '
         elif i in 'Y':
-            s += '26'
+            s += '25 '
         elif i in 'Z':
-            s += '27'
+            s += '26 '
     return s
+
+
+def _decode(_string):
+    s = ''
+    splitted = _string.split()
+    for i in splitted:
+        if i in '00':
+            s += ''
+        elif i in '01':
+            s += 'A'
+        elif i in '02':
+            s += 'B'
+        elif i in '03':
+            s += 'C'
+        elif i in '04':
+            s += 'D'
+        elif i in '05':
+            s += 'E'
+        elif i in '06':
+            s += 'F'
+        elif i in '07':
+            s += 'G'
+        elif i in '08':
+            s += 'H'
+        elif i in '09':
+            s += 'I'
+        elif i in '10':
+            s += 'J'
+        elif i in '11':
+            s += 'K'
+        elif i in '12':
+            s += 'L'
+        elif i in '13':
+            s += 'M'
+        elif i in '14':
+            s += 'N'
+        elif i in '15':
+            s += 'O'
+        elif i in '16':
+            s += 'P'
+        elif i in '17':
+            s += 'Q'
+        elif i in '18':
+            s += 'R'
+        elif i in '19':
+            s += 'S'
+        elif i in '20':
+            s += 'T'
+        elif i in '21':
+            s += 'U'
+        elif i in '22':
+            s += 'V'
+        elif i in '23':
+            s += 'W'
+        elif i in '24':
+            s += 'X'
+        elif i in '25':
+            s += 'Y'
+        elif i in '26':
+            s += 'Z'
+    return s
+"""
--- Main.py
+++ Main.py
@@ -8,30 +8,30 @@ agencies = LinkedList()
 services = LinkedList()
 
 print(30 * ' ' + 20 * '*')
-print('Note1: Type "EXIT" when you wanted to quit.')
+print('Type "exit" when you wanted to quit.')
 print(30 * ' ' + 20 * '*')
-print('Note2: Type "HELP" for allowed commands.')
+print('Type "help" for allowed commands.')
 print(30 * ' ' + 20 * '*')
 
 while True:
-    inp = input('Enter a command: ').split()
+    _input = input('Enter a command: ').split()
     try:
-        if len(inp) is 0:
-            print("Wrong!")
-        elif 'EXIT' in inp[0]:
+        if len(_input) is 0:
+            print('Wrong!\ncheck "help" for more information!')
+        elif 'exit' in _input[0]:
             break
-        elif 'HELP' in inp[0]:
+        elif 'help' in _input[0]:
             print(30 * ' ' + 20 * '*')
             print('add an agency : \n -> add agency <agency_name>\n', 30 * ' ' + 20 * '*')
-            print('add a service : \n -> add agency <service_name>\n', 30 * ' ' + 20 * '*')
+            print('add a service : \n -> add service <service_name>\n', 30 * ' ' + 20 * '*')
             print('add a subservice to a service : \n -> add subservice <subservice_name> to <service_name>\n',
                   30 * ' ' + 20 * '*')
             print('add a service to an agency : \n -> add offer <service_name> to <agency_name>\n',
                   30 * ' ' + 20 * '*')
-            print('list all available services : \n -> list services\n', 30 * ' ' + 20 * '*')
-            print('list all services of a particular service : \n -> list services from <service_name>\n',
+            print("list all available services(It's not ready yet!) : \n -> list services\n", 30 * ' ' + 20 * '*')
+            print("listAllServices of a particular service(It's not ready yet!)\n->list services from <service_name>\n",
                   30 * ' ' + 20 * '*')
-            print('list all agencies : \n -> list agencies\n', 30 * ' ' + 20 * '*')
+            print("list all agencies(It's not ready yet!) : \n -> list agencies\n", 30 * ' ' + 20 * '*')
             print('list all requests from an agency : \n -> list orders of <agency_name>\n', 30 * ' ' + 20 * '*')
             print('delete a service from an agency : \n -> delete <service_name> from <agency_name>\n',
                   30 * ' ' + 20 * '*')
@@ -39,39 +39,36 @@ while True:
                   '<customer_name> with <immediacy_level> priority\n', 30 * ' ' + 20 * '*')
             print('priority levels; choose one of them for sending request :\n\t1. high\n\t2. normal\n\t3. low')
 
-        elif inp[0] == 'add':
+        elif _input[0] == 'add':
 
-            if inp[1] in 'service':
-                services.add_service(inp[2])
-            elif inp[1] in 'subservice':
-                services.add_subservice3(inp[2], inp[4])
-            elif inp[1] in 'agency':
-                agencies.add_agency(inp[2])
-            elif inp[1] in 'offer':
-                agencies.add_offer(inp[2], inp[4])
+            if _input[1] in 'service':
+                services.add_service(_input[2])
+            elif _input[1] in 'subservice':
+                services.add_sub_service(_input[2], _input[4])
+            elif _input[1] in 'agency':
+                agencies.add_agency(_input[2])
+            elif _input[1] in 'offer':
+                agencies.add_offer(_input[2], _input[4])
             else:
-                print('Wrong command!\nCheck out the instruction with "HELP" command.')
+                print('Wrong!\ncheck "help" for more information!')
 
-        elif inp[0] == 'delete':
-            agencies.delete(inp[1], inp[3])
-        elif inp[0] == 'list':
-            if inp[1] in '‫‪agencies‬‬':
-                agencies.list_agencies()
-            elif inp[1] in '‫‪services‬‬' and len(inp) > 2 and inp[2] in 'from':
-                services.list_services(inp[3])
+        elif _input[0] == 'delete':
+            agencies.delete(_input[1], _input[3])
+        elif _input[0] == 'list':
+            if _input[1] in '‫‪agencies‬‬':
+                print("It's not ready yet!")
+            elif _input[1] in '‫‪services‬‬':
+                print("It's not ready yet!")
 
-            elif inp[1] in '‫‪services‬‬':
-                services.list_services()
-
-            elif inp[1] in 'orders':
-                agencies.list_orders(inp[3])
+            elif _input[1] in 'orders':
+                agencies.orders_list(_input[3])
             else:
-                print('Wrong command!\nCheck out the instruction with "HELP" command.')
+                print('Wrong!\ncheck "help" for more information!')
 
-        elif inp[0] == 'order':
-            agencies.order(inp[1], inp[3], inp[5], inp[7])
+        elif _input[0] == 'order':
+            agencies.order(_input[1], _input[3], _input[5], _input[7])
         else:
-            print('Wrong command!\nCheck out the instruction with "HELP" command.')
+            print('Wrong!\ncheck "help" for more information!')
         print(30 * ' ' + 20 * '*')
     except:
-        print('Wrong!\nCheck out the instruction with "HELP" command.')
+        print('Wrong!\ncheck "help" for more information!')
